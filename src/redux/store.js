ce88296b283462c1e417de87e59427b98774397a
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-15 21:15:06 +0530

    Added some comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './slices/todoSlice';
-
+// configureStore creates global data store and also the states inside the global data store.
 const store = configureStore({
+    // Below basically creates state todos is the key name of the state in the global store.
+//     {
+//    todos: // whatever todoReducer manages
+//     }
     reducer: {
         todos: todoReducer
     },
